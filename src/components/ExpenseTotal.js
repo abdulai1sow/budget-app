1247fdfa29f6ac363c0d27274dbaf13b157a6585
sowabdulai <abdulai-as@hotmail.com>
2021-04-23 09:19:40 -0400

    expense list

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ExpenseTotal = () => {
   return (
     <div className='alert alert-primary'>
-      <span>Spent so far</span>
+      <span>Spent so far $1000</span>
     </div>
   )
 }
